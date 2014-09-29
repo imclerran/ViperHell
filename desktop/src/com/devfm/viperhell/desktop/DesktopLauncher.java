e73064c8b7a874dc9ef07db795f9e980d67126c7
Ian McLerran <imclerran@gmail.com>
2014-09-29 13:32:11 -0700

    Initial Commit
    Basic LibGDX project

---
+++ desktop/src/com/devfm/viperhell/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.devfm.viperhell.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.devfm.viperhell.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
